c1b9322a55bd91a5a2c4ab106fa6b713770c73a2
aa-ag <aaron.aguerrevere@gmail.com>
2020-06-09 08:56:19 -0500

    Final

--- js/main.js
+++ js/main.js
@@ -38,17 +38,17 @@ console.log(findWords())
 /* Write a fucntion that takes in an array and removes every even index with a splice,
 and replaces it with the string "even index" */
 
-// arr = ['Max','Baseball','Reboot','Goku','Trucks','Rodger']
+arr = ['Max','Baseball','Reboot','Goku','Trucks','Rodger']
 
-// function replaceEvenIndexes(arr){
-//     for(let i = 0; i < arr.length; i++){
-//         if(i % 2 == 0){
-//             arr.splice(i, 1, 'even index')
-//         }
-//         console.log(arr)
-//     }
-// }
-// replaceEvenIndexes(arr)
+function replaceEvenIndexes(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(i % 2 == 0){
+            arr.splice(i, 1, 'even index')
+        }
+        console.log(arr)
+    }
+}
+replaceEvenIndexes(arr)
 
 // Expected output
 // Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
